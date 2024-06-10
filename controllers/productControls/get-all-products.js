11096180733aae8da0f5dacc8b952f93ee7363a4
Gagan-Saini-GS <gagansaini10999@gmail.com>
2024-06-10 18:07:35 +0530

    Reduced get all products API load by getting only required details

--- controllers/productControls/get-all-products.js
+++ controllers/productControls/get-all-products.js
@@ -2,7 +2,15 @@ const Product = require("../../models/Products");
 
 const getAllProducts = async (req, res) => {
   try {
-    const products = await Product.find();
+    const allProducts = await Product.find();
+
+    const products = await allProducts.map((product) => ({
+      id: product._id,
+      name: product.name,
+      price: product.price,
+      productImg: product.productImg,
+    }));
+
     res.status(200).json(products);
   } catch (error) {
     res.status(500).json({ error: error.message });
